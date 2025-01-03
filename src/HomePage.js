861751fa0defcc73b5ae1e60628c22aa377221d3
Joyion Timmons <joyiont@gmail.com>
2025-01-03 00:15:39 -0600

    Creates home page layout and adds images

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,50 +6,154 @@ import GuitarImg from "../assets/pexels-yankrukov-9009574.jpg";
 export const HomePage = () => {
     return (
         <div>
-            <div className="container">
+            <section className="title-container">
                 <div className="main-title">
                     <h1>Black Rose Records</h1>
                 </div>
-
-                <p className="sub-title">Jazz. Electronic. Classical</p>
-
                 <div className="img-collage-container">
-                    <img src={SaxPlayerImg} />
                     <img src={DjImg} />
+                    <img src={SaxPlayerImg} />
                     <img src={GuitarImg} />
                 </div>
-                <div className="main-title">
-                    <h2>we're a <span>Record Label, Publisher</span> and <span>Music Library</span></h2>
-                    {/* <h1 className="main-title">Publisher <span>and</span></h1> */}
-                    <h2>specliazing in <span>JAZZ, Eletronic </span>and <span>Classical music</span></h2>
-                    <h2>with <span>thousand of songs</span> from...</h2>
-                    {/* <h1 className="main-title">of songs from...</h1> */}
+            </section>
+
+            <section className="featured-container">
+                <section className="featured-artists">
+                    <h2>Artists Who Push Boundaries</h2>
+                </section>
+            </section>
+
+
+
+            <section className="card-section">
+                <div className="title-sect">
+                    <div className="subtitle-block">
+                        <div>
+                            <h2>Latest News</h2>
+                        </div>
+                    </div>
                 </div>
 
-            </div>
-            <div className="dark-container">
-                <h1>Artists Who Push Boundaries</h1>
-            </div>
-            <div>
-                <h2>Latest News</h2>
-                <ul>
-                    <li>Article Card 1</li>
-                    <li>Article Card 2</li>
-                    <li>Article Card 3</li>
-                </ul>
-            </div>
-            <div>
-                <h2>Latest Releases</h2>
-                <ul>
-                    <li>Article Card 1</li>
-                    <li>Article Card 2</li>
-                    <li>Article Card 3</li>
-                </ul>
-            </div>
-            <div>
-                <h2>Search Our Catalog</h2>
-            </div>
-        </div>
+                <div className="cards">
+                    <ul>
+                        <li>
+                            <div>
+                                <h4>NeverLand discusses their new album Renegade</h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Kiss X favorite releases this year</h4>
+
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Antonio's favorite guitar solos</h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>NeverLand discusses their new album Renegade</h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Kiss X favorite releases this year</h4>
+
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Antonio's favorite guitar solos</h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section >
+
+
+            <section className="card-section">
+                <div className="title-sect">
+                    <div className="subtitle-block">
+                        <div>
+                            <h2>Latest Releases</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="cards">
+                    <ul>
+                        <li>
+                            <div>
+                                <h4>Renegade</h4>
+                                <p>Neverland</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Lost At Sea</h4>
+                                <p>Xavi</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Dark Is The Night</h4>
+                                <p>Twin Towers</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Renegade</h4>
+                                <p>Neverland</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Lost At Sea</h4>
+                                <p>Xavi</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <h4>Dark Is The Night</h4>
+                                <p>Twin Towers</p>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </section>
+
+            <section className="search-container">
+                <div className="title-container">
+                    <h2>Find the perfect song</h2>
+                    <div>
+                        <p>Whether it's for your film, podcast, or just for your own listening pleasure.
+                            Our catalog has thousands of songs to choose from many genres.
+                            We also have artists available to compose music for any type of project
+                        </p>
+                    </div>
+
+                    <div>
+                        <form>
+                            <input
+                                aria-label="song-search"
+                                name="song-search"
+                                placeholder="Search by Artists, Track Name, Genre or mood"
+                            />
+                            <button aria-label="find songs" type="submit">Find Songs</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+            <footer>
+                <p><a href="#">Contact Us</a> | <a href="#">Demo Submission</a> </p>
+                <p>Black Rose Records@2025</p>
+            </footer>
+
+        </div >
 
     )
 }
--- src/PageContainer.js
+++ src/PageContainer.js
@@ -3,20 +3,47 @@
 // import "./styles/components.scss";
 
 import { Outlet } from "react-router-dom"
+import RoseLogo from "../assets/rose-shape.png";
 
 
 export const PageContainer = () => {
     return (
         <div>
             <div className="navbar">
-                <p>Black Rose Records</p>
 
-                <ul>
-                    <li>News</li>
-                    <li>Artists</li>
-                    <li>Music </li>
-                    <li>Our Story</li>
-                </ul>
+                <div className="nav-links">
+                    <ul>
+                        <li><a href="#"><img src={RoseLogo} /></a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Our Artists</a></li>
+                        <li><a href="#">Our Catalog</a></li>
+                        <li><a href="#">Our Story</a></li>
+                    </ul>
+                </div>
+
+
+
+                <div className="search-bar">
+                    <div>
+
+                    </div>
+
+                    <uL>
+                        <li>
+                            <form>
+                                <input placeholder="" />
+                                <button type="submit">Search</button>
+                            </form>
+                        </li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                    </uL>
+                </div>
+
+
+
             </div>
             <Outlet />
 
